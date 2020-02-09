6f6158163a40180f5056817edfdf98bba0c7012b
Johnpaul Vangerwua <torsxperience@outlook.com>
2020-02-09 21:36:41 +0100

    Update uniques keys

---
+++ src/templates/indexTemplate.js
@@ -0,0 +1,17 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+export default class IndexTemplate extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return <div>Hi There</div>
+  }
+}
+
+export const indexPageQuery = graphql`
+    
+
+`;
