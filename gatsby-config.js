62f8a14f362a46d965d9614980443ac9c9c93301
Johnpaul Vangerwua <torsxperience@outlook.com>
2020-02-09 19:57:02 +0100

    Fixed cms config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,15 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-netlify-cms`],
+  plugins: [
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: `${__dirname}/src/pages`
+      }
+    },
+  ],
 }
